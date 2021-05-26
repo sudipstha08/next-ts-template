5e546b1a69a28f8200c2d6a482ebb28be95fbd57
Sudip Shrestha <sudipstha08@gmail.com>
2021-05-26 12:46:35 +0545

    :book: docs: update readme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import '../styles/globals.css'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Next | Template</title>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
+  )
 }
+
 export default MyApp
