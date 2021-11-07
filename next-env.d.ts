7be52ae29e69488d85fa7f3ece6a6808cac74469
Sudip Shrestha <sudipstha08@gmail.com>
2021-11-07 21:57:11 +0545

    chore: allow svg import

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+// Allow SVG import as a module
+declare module '*.svg' {
+  export default JSX.IntrinsicElements.svg
+}
