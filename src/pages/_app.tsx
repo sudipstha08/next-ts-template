6eea7dd661edaba7f52b7fe76f990b62ebb87e9a
Sudip Shrestha <sudipstha08@gmail.com>
2022-02-23 00:14:11 +0545

    feat: add linters

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <QueryClientProvider client={queryClient}>
         <GlobalStyles />
-        {tests}
         <Component {...pageProps} />
       </QueryClientProvider>
     </>
