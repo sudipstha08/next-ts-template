05a5764298ec10c31fad07802ce54843f3df7d72
Sudip Shrestha <sudipstha08@gmail.com>
2022-02-22 23:57:08 +0545

    feat: update document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,36 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
   render() {
     return (
       <Html>
         <Head>
-          <title>Next | Template</title>
+          <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
@@ -20,5 +40,3 @@ class MyDocument extends Document {
     )
   }
 }
-
-export default MyDocument
