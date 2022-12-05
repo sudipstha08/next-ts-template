6f8ca8ae5b77c7795cc4bc79a8ffe63c66ad4a4a
sudipstha08 <sudipstha088@gmail.com>
2022-12-06 01:00:09 +0545

    chore: update config

--- babel.config.js
+++ babel.config.js
@@ -7,11 +7,15 @@ module.exports = {
       {
         root: ['./'],
         alias: {
-          '@src': './src',
           '@public': './public',
           '@components': './src/components',
           '@services': './src/services',
           '@utils': './src/utils',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@interfaces': './src/interfaces',
+          '@store': './src/store',
+          '@src': './src',
         },
         cwd: 'babelrc',
         extensions: ['.js', '.json', '.ts', '.tsx'],
