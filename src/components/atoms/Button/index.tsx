1acc3d570e30995a1f4df07e2b46ee95dfb927d4
sudipstha08 <sudipstha08@gmail.com>
2024-06-18 10:24:12 +0545

    add packages

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+interface IButton    {
+  label: string
+}
+
+export const Button: FC<IButton> = ({ label }) => {
+  return <button>{label}</button>
+}
---
+++ src/constants/globals.ts
@@ -0,0 +1,1 @@
+export const APP_TITLE = 'Next 14 TS Template'
