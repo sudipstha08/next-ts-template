8b05590ee0ced5e607d7f99333d58684290acb79
sudipstha08 <sudipstha08@gmail.com>
2024-06-19 13:55:01 +0545

    chore: update configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       backgroundImage: {
@@ -17,4 +13,5 @@ const config: Config = {
   },
   plugins: [],
 }
+
 export default config
